267bc4cfedafec42bfc7eab114b8146dfe9ca826
ayushi2233 <35265929+ayushi2233@users.noreply.github.com>
2018-01-10 23:03:44 +0530

    ayushi ques1

--- guvi_set1_ques1.py
+++ guvi_set1_ques1.py
@@ -1,7 +1,13 @@
-a = int(input())
-if(a > 0):
-    print("Positive")
-if(a < 0):
-    print("Negative")
-if(a == 0):
-    print("Zero")
+import re
+pat = r'-?[0-9]+'
+a = input()
+if bool(re.match(pat,a)):
+    a = int(a)
+    if(a > 0):
+        print("Positive")
+    if(a < 0):
+        print("Negative")
+    if(a == 0):
+        print("Zero")
+else:
+    print("invalid input")
