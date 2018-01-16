b89bc1282e912d0aba23cac524f18462607aadc6
ayushi2233 <35265929+ayushi2233@users.noreply.github.com>
2018-01-16 22:45:25 +0530

    player set1 ques2

--- player_set1_ques2.py
+++ player_set1_ques2.py
@@ -1,14 +1,16 @@
 import re
-pat = r'^[+]?[0-9]+$'
+pat1 = r'^[+]?[0-9]+$'
+pat2 = r'^[-]?[0-9]+$'
 n = input()
-if bool(re.match(pat,n)):
+if bool(re.match(pat1,n)):
     n = int(n)
     f = 1
     for i in range(1,n+1):
         f = f*i
-        
-        
     print(f)
+elif bool(re.match(pat2,n)):
+    print(0)
+        
 else:
     print("invalid input")
         
